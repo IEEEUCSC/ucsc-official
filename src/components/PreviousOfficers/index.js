043b2c84e33b15ddd178071e5fbb7e25d9dae8c6
Nipun Chamikara <nipunchamikara@yahoo.com>
2022-07-10 13:18:41 +0530

    minor changes to Previous Officers section

--- src/components/PreviousOfficers/index.js
+++ src/components/PreviousOfficers/index.js
@@ -15,7 +15,7 @@ function PreviousTeam() {
 
   return (
     <Container
-      className="previous-teams mt-5 mb-5 pt-2 pt-md-2 pt-lg-3 pt-xxl-5 pb-1 px-lg-5 px-xxl-5"
+      className="previous-officers mt-5 mb-5 pt-2 pt-md-2 pt-lg-3 pt-xxl-5 pb-1 px-lg-5 px-xxl-5"
       as="section"
     >
       <div className="my-5 pb-1 pb-lg-3 mx-2 mx-md-3 mx-lg-5">
